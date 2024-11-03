d599c5e53c45b7cb92330d20b23ed082a7343dfd
AndersonPaim <andersonpaim15@outlook.com>
2024-11-03 19:12:42 -0300

    Added campfire fire duration

--- Trijam294/Assets/Scripts/CampFire.cs
+++ Trijam294/Assets/Scripts/CampFire.cs
@@ -10,16 +10,31 @@ public class CampFire : MonoBehaviour
     [SerializeField] private Animator _animator;
     [SerializeField] private bool _startActivated;
     [SerializeField] private int _lightIntensity;
+    [SerializeField] private int _fireDuration;
 
     public bool IsActivated => _isActivated;
 
+    private Tween _torchTween;
     private bool _isActivated;
+    private float _currentFireDuration;
 
     public void ActivateCampFire()
     {
+        _torchTween.Kill();
         _animator.SetTrigger("Play");
+        _currentFireDuration = _fireDuration;
         _isActivated = true;
-        DOTween.To(() => _light.intensity, x => _light.intensity = x, _lightIntensity, 4);
+
+        DOTween.To(() => _light.intensity, x => _light.intensity = x, _lightIntensity, 4).OnComplete
+        (
+            () => _torchTween = DOTween.To(() => _light.intensity, x => _light.intensity = x, 0, _currentFireDuration).OnComplete(DeactivateCampFire)
+        );
+    }
+
+    private void DeactivateCampFire()
+    {
+        _isActivated = false;
+        _animator.SetTrigger("Stop");
     }
 
     private void Start()
@@ -27,6 +42,14 @@ public class CampFire : MonoBehaviour
         if (_startActivated)
         {
             ActivateCampFire();
+        }
+    }
+
+    private void Update()
+    {
+        if (_isActivated)
+        {
+            _currentFireDuration -= Time.deltaTime;
         }
     }
 }
