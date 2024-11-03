6121046654272a82adc23f417e9d22d96cc3b65b
AndersonPaim <andersonpaim15@outlook.com>
2024-11-03 18:52:24 -0300

    Added environment props

--- Trijam294/Assets/Scripts/CampFire.cs
+++ Trijam294/Assets/Scripts/CampFire.cs
@@ -7,6 +7,7 @@ using UnityEngine.Rendering.Universal;
 public class CampFire : MonoBehaviour
 {
     [SerializeField] private Light2D _light;
+    [SerializeField] private Animator _animator;
     [SerializeField] private bool _startActivated;
     [SerializeField] private int _lightIntensity;
 
@@ -16,8 +17,9 @@ public class CampFire : MonoBehaviour
 
     public void ActivateCampFire()
     {
+        _animator.SetTrigger("Play");
         _isActivated = true;
-        DOTween.To(() => _light.intensity, x => _light.intensity = x, _lightIntensity, 2);
+        DOTween.To(() => _light.intensity, x => _light.intensity = x, _lightIntensity, 4);
     }
 
     private void Start()
